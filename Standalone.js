f8c4d034e93f74c7734a4aa833bd0b0ff233ee11
@Finite-Code <finitecode.contact@gmail.com>
2021-12-01 20:23:30 +0530

    Add files via upload

---
+++ Standalone.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    function changeStyle(){
+        var element = document.getElementById("Yio");
+        element.style.fontSize = "20px";
+    }
